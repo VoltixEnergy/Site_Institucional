2d12fe51f7c35d5e61d2cffe7d60e4e15c0149b7
Daniel Bueno <111004405+danielsrbueno@users.noreply.github.com>
2026-09-16 22:11:26 -0300

    chore(project-structure): standardize the directory hierarchy

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -8,13 +8,13 @@ function validarSessao() {
     if (email != null && nome != null) {
         b_usuario.innerHTML = nome;
     } else {
-        window.location = "../login.html";
+        window.location = "../login";
     }
 }
 
 function limparSessao() {
     sessionStorage.clear();
-    window.location = "../login.html";
+    window.location = "../login";
 }
 
 // carregamento (loading)
