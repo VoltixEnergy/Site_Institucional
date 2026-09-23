81403b2756b3656745a6dd79baeb89d412b99131
Daniel Bueno <111004405+danielsrbueno@users.noreply.github.com>
2026-09-23 20:20:57 -0300

    refactor(user): standardize user controller code

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,251 +1,288 @@
-var usuarioModel = require("../models/usuarioModel");
+const usuarioModel = require("../models/usuarioModel");
 
-function autenticar(req, res) {
-  var email = req.body.emailServer;
-  var senha = req.body.senhaServer;
+async function autenticar(req, res) {
+  const { email, senha } = req.body
 
-  if (email == undefined) {
-    res.status(400).send("Seu email está undefined!");
-  } else if (senha == undefined) {
-    res.status(400).send("Sua senha está indefinida!");
-  } else {
-
-    usuarioModel.autenticar(email, senha)
-      .then(
-        function (resultadoAutenticar) {
-          console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
-          console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
-          console.log(resultadoAutenticar[0])
-          if (resultadoAutenticar.length == 1) {
-            console.log(resultadoAutenticar);
-
-            usuarioModel.buscarUsuarioPorEmpresa(resultadoAutenticar[0].fk_empresa)
-              .then((resultadoFuncionarios) => {
-                if (resultadoFuncionarios.length > 0) {
-                  res.json({
-                    id: resultadoAutenticar[0].id_usuario,
-                    email: resultadoAutenticar[0].email,
-                    nome: resultadoAutenticar[0].nome,
-                    // senha: resultadoAutenticar[0].senha,
-                    empresa: resultadoAutenticar[0].fk_empresa,
-                    funcionarios: resultadoFuncionarios
-                  });
-                } else {
-                  res.status(204).json({ funcionarios: [] });
-                }
-              })
-            // res.json({
-            //     id: resultadoAutenticar[0].id,
-            //     email: resultadoAutenticar[0].email,
-            //     nome: resultadoAutenticar[0].nome,
-            //     senha: resultadoAutenticar[0].senha,
-            //     empresa: resultadoAutenticar[0].fk_empresa
-            // });
-          } else if (resultadoAutenticar.length == 0) {
-            res.status(403).send("Email e/ou senha inválido(s)");
-          } else {
-            res.status(403).send("Mais de um usuário com o mesmo login e senha!");
-          }
-        }
-      ).catch(
-        function (erro) {
-          console.log(erro);
-          console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
-          res.status(500).json(erro.sqlMessage);
-        }
-      );
+  const retorno = {
+    mensagem: "",
+    dados: {}
   }
 
-}
-function buscarUsuarioPorCPF(req, res) {
-
-  if (resultado.length > 0) {
-    res
-      .status(401)
-      .json({ mensagem: `O usuário com o CPF ${cpf} já existe` });
-  } else {
-    usuarioModel.cadastrar(nome, email, senha, cpf, nivelPermissao, fkEmpresa)
-      .then(
-        function (resultado) {
-          res.json(resultado);
-        }
-      ).catch(
-        function (erro) {
-          console.log(erro);
-          console.log(
-            "\nHouve um erro ao realizar o cadastro! Erro: ",
-            erro.sqlMessage
-          );
-          res.status(500).json(erro.sqlMessage);
-        }
-      );
+  if (!email) {
+    retorno.mensagem = "Email inválido."
+    return res.status(400).json(retorno)
   }
-};
+  if (!senha) {
+    retorno.mensagem = "Senha inválida."
+    return res.status(400).json(retorno)
+  }
 
-function cadastrar(req, res) {
-  // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-  var nome = req.body.nomeServer;
-  var email = req.body.emailServer;
-  var senha = req.body.senhaServer;
-  var cpf = req.body.cpfServer;
-  var nivelPermissao = req.body.nivelPermissaoServer;
-  var fkEmpresa = req.body.fkEmpresaServer;
+  try {
+    const resultadoAutenticar = await usuarioModel.autenticar(email, senha)
 
-  // Faça as validações dos valores
-  if (nome == undefined) {
-    res.status(400).send("Seu nome está undefined!");
-  } else if (email == undefined) {
-    res.status(400).send("Seu email está undefined!");
-  } else if (senha == undefined) {
-    res.status(400).send("Sua senha está undefined!");
-  } else {
+    if (resultadoAutenticar.length === 1) {
+      const resultadoFuncionarios = await usuarioModel.buscarUsuarioPorEmpresa(resultadoAutenticar[0].fk_empresa)
 
-    // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-    usuarioModel.buscarUsuarioPorCPF(cpf).then((resultado) => {
-      if (resultado.length > 0) {
-        res
-          .status(401)
-          .json({ mensagem: `O usuário com o CPF ${cpf} já existe` });
-      } else {
-        usuarioModel.cadastrar(nome, email, senha, cpf, nivelPermissao, fkEmpresa)
-          .then(
-            function (resultado) {
-              res.json(resultado);
-            }
-          ).catch(
-            function (erro) {
-              console.log(erro);
-              console.log(
-                "\nHouve um erro ao realizar o cadastro! Erro: ",
-                erro.sqlMessage
-              );
-              res.status(500).json(erro.sqlMessage);
-            }
-          );
+      retorno.dados = {
+        id: resultadoAutenticar[0].id_usuario,
+        email: resultadoAutenticar[0].email,
+        nome: resultadoAutenticar[0].nome,
+        empresa: resultadoAutenticar[0].fk_empresa,
+        funcionarios: resultadoFuncionarios.length > 0 ? resultadoFuncionarios : []
       }
-    });
-  }
 
-}
-
-function autenticarCodigo(req, res) {
-  var codigo = req.body.codigoServer;
-
-  if (codigo == undefined) {
-    res.status(400).send("Seu codigo está undefined!");
-  } else {
-
-    usuarioModel.autenticarCodigo(codigo)
-      .then(
-        function (resultadoAutenticarCodigo) {
-          console.log(`\nResultados encontrados: ${resultadoAutenticarCodigo.length}`);
-          console.log(`Resultados: ${JSON.stringify(resultadoAutenticarCodigo)}`); // transforma JSON em String
-
-          if (resultadoAutenticarCodigo.length == 1) {
-            console.log(resultadoAutenticarCodigo);
-            res.json({
-              id: resultadoAutenticarCodigo[0].id_codigo,
-              codigo: resultadoAutenticarCodigo[0].codigo,
-              status: resultadoAutenticarCodigo[0].estado_codigo,
-            });
-          } else if (resultadoAutenticarCodigo.length == 0) {
-            res.status(403).send("Código inválido!");
-          } else {
-            res.status(403).send("Mais de um usuário com o mesmo login e senha!???");
-          }
-        }
-      ).catch(
-        function (erro) {
-          console.log(erro);
-          console.log("\nHouve um erro ao validar o código! Erro: ", erro.sqlMessage);
-          res.status(500).json(erro.sqlMessage);
-        }
-      );
-  }
-
-}
-
-function buscarUsuarioPorEmpresa(req, res) {
-  console.log("controller")
-  var idEmpresa = req.params.idEmpresa;
-  console.log(idEmpresa)
-  usuarioModel.buscarUsuarioPorEmpresa(idEmpresa).then((resultado) => {
-    if (resultado.length > 0) {
-      res.status(200).json(resultado);
+      return res.status(200).json(retorno)
+    } else if (resultadoAutenticar.length === 0) {
+      retorno.mensagem = "Email e/ou senha inválido(s)"
+      return res.status(403).json(retorno)
     } else {
-      res.status(204).json([]);
+      retorno.mensagem = "Mais de um usuário com o mesmo login e senha!"
+      return res.status(403).json(retorno)
     }
-  }).catch(function (erro) {
-    console.log(erro);
-    console.log("Houve um erro ao buscar os funcionarios: ", erro.sqlMessage);
-    res.status(500).json(erro.sqlMessage);
-  });
+  } catch (erro) {
+    console.error(erro)
+    retorno.mensagem = "Algo deu errado. Tente novamente mais tarde."
+    return res.status(500).json(retorno)
+  }
 }
-function adicionarCodigo(req, res) {
-  // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-  var codigo = req.body.codigoServer;
+async function buscarUsuarioPorCPF(req, res) {
+  const { cpf } = req.body
 
-  // Faça as validações dos valores
-  if (codigo == undefined) {
-    res.status(400).send("Seu codigo está undefined!");
-  } else {
+  const retorno = {
+    mensagem: "",
+    dados: {}
+  }
 
-    // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-    usuarioModel.adicionarCodigo(codigo)
-      .then(
-        function (resultado) {
-          res.json(resultado);
-        }
-      ).catch(
-        function (erro) {
-          console.log(erro);
-          console.log(
-            "\nHouve um erro ao realizar o cadastro do código! Erro: ",
-            erro.sqlMessage
-          );
-          res.status(500).json(erro.sqlMessage);
-        }
-      );
+  if (!cpf) {
+    retorno.mensagem = "CPF inválido."
+    return res.status(400).json(retorno)
+  }
+
+  try {
+    const resultado = await usuarioModel.buscarUsuarioPorCPF(cpf)
+
+    retorno.dados = resultado
+    retorno.mensagem = resultado.length ? "" : "Nenhum usuário encontrado."
+
+    return res.status(200).json(retorno)
+  } catch (erro) {
+    console.error(erro)
+    retorno.mensagem = "Algo deu errado. Tente novamente mais tarde."
+    return res.status(500).json(retorno)
   }
 }
 
-function editarNome(req, res) {
-  var novoNome = req.body.novoNome;
-  var idusuario = req.body.idUsuario;
+async function cadastrar(req, res) {
+  const { nome, email, senha, cpf, nivelPermissao, fkEmpresa } = req.body
 
-  usuarioModel.editarNome(novoNome, idusuario)
-    .then(
-      function (resultado) {
-        res.json(resultado);
-      }
-    )
-    .catch(
-      function (erro) {
-        console.log(erro);
-        console.log("Houve um erro ao editar o nome: ", erro.sqlMessage);
-        res.status(500).json(erro.sqlMessage);
-      }
-    );
+  const retorno = {
+    mensagem: "",
+    dados: {}
+  }
 
+  if (!nome) {
+    retorno.mensagem = "Nome inválido."
+    return res.status(400).json(retorno)
+  }
+  if (!email) {
+    retorno.mensagem = "Email inválido."
+    return res.status(400).json(retorno)
+  }
+  if (!senha) {
+    retorno.mensagem = "Senha inválida."
+    return res.status(400).json(retorno)
+  }
+
+  try {
+    const usuarioExistente = await usuarioModel.buscarUsuarioPorCPF(cpf)
+
+    if (usuarioExistente.length > 0) {
+      retorno.mensagem = `O usuário já existe`
+      return res.status(400).json(retorno)
+    }
+
+    const resultado = await usuarioModel.cadastrar(nome, email, senha, cpf, nivelPermissao, fkEmpresa)
+
+    if (resultado.affectedRows !== 1) {
+      retorno.mensagem = "O usuário não foi cadastrado. Tente novamente mais tarde."
+      return res.status(500).json(retorno)
+    }
+
+    retorno.mensagem = "Usuário cadastrado com sucesso!"
+    retorno.dados = {
+      id: resultado.insertId
+    }
+
+    return res.status(200).json(retorno)
+  } catch (erro) {
+    console.error(erro)
+    retorno.mensagem = "Algo deu errado. Tente novamente mais tarde."
+    return res.status(500).json(retorno)
+  }
 }
 
-function deletarUsuario(req, res) {
-  var idUsuario = req.params.idUsuario;
+async function autenticarCodigo(req, res) {
+  const { codigoServer: codigo } = req.body
 
-  usuarioModel.deletarUsuario(idUsuario)
-    .then(
-      function (resultado) {
-        res.json(resultado);
-      }
-    )
+  const retorno = {
+    mensagem: "",
+    dados: {}
+  }
 
-    .catch(
-      function (erro) {
-        console.log(erro);
-        console.log("Houve um erro ao deletar o usuário: ", erro.sqlMessage);
-        res.status(500).json(erro.sqlMessage);
+  if (!codigo) {
+    retorno.mensagem = "Código inválido."
+    return res.status(400).json(retorno)
+  }
+
+  try {
+    const resultadoAutenticarCodigo = await usuarioModel.autenticarCodigo(codigo)
+
+    if (resultadoAutenticarCodigo.length === 1) {
+      retorno.dados = {
+        id: resultadoAutenticarCodigo[0].id_codigo,
+        codigo: resultadoAutenticarCodigo[0].codigo,
+        status: resultadoAutenticarCodigo[0].estado_codigo
       }
-    );
+
+      return res.status(200).json(retorno)
+    } else if (resultadoAutenticarCodigo.length === 0) {
+      retorno.mensagem = "Código inválido!"
+      return res.status(403).json(retorno)
+    } else {
+      retorno.mensagem = "Mais de um código com o mesmo valor!"
+      return res.status(403).json(retorno)
+    }
+  } catch (erro) {
+    console.error(erro)
+    retorno.mensagem = "Algo deu errado. Tente novamente mais tarde."
+    return res.status(500).json(retorno)
+  }
+}
+
+async function buscarUsuarioPorEmpresa(req, res) {
+  const { idEmpresa } = req.params
+
+  const retorno = {
+    mensagem: "",
+    dados: {}
+  }
+
+  if (!idEmpresa) {
+    retorno.mensagem = "ID da empresa inválido."
+    return res.status(400).json(retorno)
+  }
+
+  try {
+    const resultado = await usuarioModel.buscarUsuarioPorEmpresa(idEmpresa)
+
+    retorno.dados = resultado
+    retorno.mensagem = resultado.length ? "" : "Nenhum funcionário encontrado."
+
+    return res.status(200).json(retorno)
+  } catch (erro) {
+    console.error(erro)
+    retorno.mensagem = "Algo deu errado. Tente novamente mais tarde."
+    return res.status(500).json(retorno)
+  }
+}
+async function adicionarCodigo(req, res) {
+  const { codigo } = req.body
+
+  const retorno = {
+    mensagem: "",
+    dados: {}
+  }
+
+  if (!codigo) {
+    retorno.mensagem = "Código inválido."
+    return res.status(400).json(retorno)
+  }
+
+  try {
+    const resultado = await usuarioModel.adicionarCodigo(codigo)
+
+    if (resultado.affectedRows !== 1) {
+      retorno.mensagem = "O código não foi adicionado. Tente novamente mais tarde."
+      return res.status(500).json(retorno)
+    }
+
+    retorno.mensagem = "Código adicionado com sucesso!"
+    retorno.dados = {
+      id: resultado.insertId
+    }
+
+    return res.status(200).json(retorno)
+  } catch (erro) {
+    console.error(erro)
+    retorno.mensagem = "Algo deu errado. Tente novamente mais tarde."
+    return res.status(500).json(retorno)
+  }
+}
+
+async function editarNome(req, res) {
+  const { novoNome, idUsuario } = req.body
+
+  const retorno = {
+    mensagem: "",
+    dados: {}
+  }
+
+  if (!novoNome) {
+    retorno.mensagem = "Nome inválido."
+    return res.status(400).json(retorno)
+  }
+  if (!idUsuario) {
+    retorno.mensagem = "ID do usuário inválido."
+    return res.status(400).json(retorno)
+  }
+
+  try {
+    const resultado = await usuarioModel.editarNome(novoNome, idUsuario)
+
+    if (resultado.affectedRows && resultado.affectedRows >= 1) {
+      retorno.mensagem = "Nome editado com sucesso!"
+      retorno.dados = resultado
+      return res.status(200).json(retorno)
+    }
+
+    retorno.mensagem = "Não foi possível editar o nome."
+    return res.status(500).json(retorno)
+  } catch (erro) {
+    console.error(erro)
+    retorno.mensagem = "Algo deu errado. Tente novamente mais tarde."
+    return res.status(500).json(retorno)
+  }
+}
+
+async function deletarUsuario(req, res) {
+  const { idUsuario } = req.params
+
+  const retorno = {
+    mensagem: "",
+    dados: {}
+  }
+
+  if (!idUsuario) {
+    retorno.mensagem = "ID do usuário inválido."
+    return res.status(400).json(retorno)
+  }
+
+  try {
+    const resultado = await usuarioModel.deletarUsuario(idUsuario)
+
+    if (resultado.affectedRows && resultado.affectedRows >= 1) {
+      retorno.mensagem = "Usuário deletado com sucesso!"
+      retorno.dados = resultado
+      return res.status(200).json(retorno)
+    }
+
+    retorno.mensagem = "Não foi possível deletar o usuário."
+    return res.status(500).json(retorno)
+  } catch (erro) {
+    console.error(erro)
+    retorno.mensagem = "Algo deu errado. Tente novamente mais tarde."
+    return res.status(500).json(retorno)
+  }
 }
 
 module.exports = {
